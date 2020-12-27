9c8133ed54d818c0f10635a1087361a033650362
Bryon Kurt Segunto <bsegunto>
2020-12-27 17:06:38 +0800

    Configured Recipe array to display the recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [ ];
+  recipes: Recipe[ ] = [
+    new Recipe('Egg', 'Eggs recipe','https://bit.ly/3mRWuWW'),
+    new Recipe('Beef wellington', 'Eggs recipe','https://bit.ly/2MadsmO')
+  ];
   constructor() { }
 
   ngOnInit(): void {
